c61ea15596b92a0fadd43fb0c2fbfba3ba605576
VadymWebC <vadymcdevelweb@gmail.com>
2025-04-28 19:43:47 +0300

    add listeners to pressed keys

--- controls.js
+++ controls.js
@@ -5,4 +5,23 @@ class Controls {
 		this.right = false
 		this.reverse = false
 	}
+
+	#addKeyboardListeners() {
+		document.onkeydown = event => {
+			switch (event.key) {
+				case "ArrowLeft":
+					this.left = true
+					break
+				case "ArrowRight":
+					this.right = true
+					break
+				case "ArrowUp":
+					this.forward = true
+					break
+				case "ArrowDown":
+					this.reverse = true
+					break
+			}
+		}
+	}
 }
