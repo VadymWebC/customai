4b9258b3d02e37b141579d8315828d28d230ca4a
VadymWebC <vadymcdevelweb@gmail.com>
2025-04-29 20:01:56 +0300

    update

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function animate() {
 	car.update()
 	canvas.height = window.innerHeight
 	ctx.save()
-	ctx.translate(0, -car.y)
+	ctx.translate(0, -car.y + canvas.height * 0.7)
 	road.draw(ctx)
 	car.draw(ctx)
 	ctx.restore()
