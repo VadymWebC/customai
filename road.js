8e2ec7272857946e04a539faaeb15773c259240f
VadymWebC <vadymcdevelweb@gmail.com>
2025-04-29 17:17:56 +0300

    update

--- road.js
+++ road.js
@@ -1,5 +1,5 @@
 class Road {
-	constructor(x, width, laneCount) {
+	constructor(x, width, laneCount = 3) {
 		this.x = x
 		this.width = width
 		this.laneCount = laneCount
@@ -16,14 +16,21 @@ class Road {
 		ctx.lineWidth = 5
 		ctx.strokeStyle = "white"
 
-		ctx.beginPath()
-		ctx.moveTo(this.left, this.top)
-		ctx.lineTo(this.left, this.bottom)
-		ctx.stroke()
+		for (let i = 0; i <= this.laneCount; i++) {
+			const x = lerp(this.left, this.right, i / this.laneCount)
+			ctx.beginPath()
+			ctx.moveTo(x, this.top)
+			ctx.lineTo(x, this.bottom)
+			ctx.stroke()
+		}
 
-		ctx.beginPath()
-		ctx.moveTo(this.right, this.top)
-		ctx.lineTo(this.right, this.bottom)
-		ctx.stroke()
+		// ctx.beginPath()
+		// ctx.moveTo(this.right, this.top)
+		// ctx.lineTo(this.right, this.bottom)
+		// ctx.stroke()
 	}
+}
+
+function lerp(A, B, t) {
+	return A + (B - A) * t
 }
