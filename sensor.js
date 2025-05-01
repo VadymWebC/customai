e7361d11a65187acbdc72ec070c25ff87ef4d322
VadymWebC <vadymcdevelweb@gmail.com>
2025-05-01 23:07:55 +0300

    update

--- sensor.js
+++ sensor.js
@@ -32,6 +32,9 @@ class Sensor {
 			ctx.beginPath()
 			ctx.lineWidth = 2
 			ctx.strokeStyle = "yellow"
+			ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y)
+			ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y)
+			ctx.stroke()
 		}
 	}
 }
